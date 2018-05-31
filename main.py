3dd160d61cb76b61b5ad2fdfa09ffe3d3382f212
Jacob Chencha <jmokua@gmail.coma>
2018-05-31 07:27:54 +0300

    Part 3
    Here now able to save data in cookies.
    Added new libraries from import and pre creating the response variable

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import redirect
+from flask import make_response
+from flask import request
+
+import json
+
 app = Flask(__name__)
 
 
@@ -12,7 +17,9 @@ def index():
 
 @app.route("/save", methods=['POST'])
 def save():
-    return redirect(url_for('index'))
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('character', json.dumps(dict(request.form.items())))
+    return response
 
 
 if __name__ == '__main__':
