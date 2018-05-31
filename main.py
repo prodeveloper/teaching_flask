895159414a7fd5d1ccdda0a7fdd71bdf1cbe1efc
Chencha Jacob <jchencha@TFL-TECH-0002.local>
2018-05-31 07:03:35 +0300

    Basic form accepting

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/save", methods=['POST'])
+def save():
+    return "Saved"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
